36ac0496c41251e8a61de8d4cde00aa25655fcaa
OmkarChoudhari2303 <omkarchoudhari2003@gmail.com>
2026-05-25 20:55:17 +0530

    Fix: nodemailer to connect via ipv4 only to prevent ENETUNREACH on Render

--- server/src/utils/sendMail.js
+++ server/src/utils/sendMail.js
@@ -9,7 +9,8 @@ const transporter = nodemailer.createTransport({
         pass: process.env.EMAIL_PASS
     },
     connectionTimeout: 15000, // 15 seconds
-    greetingTimeout: 15000
+    greetingTimeout: 15000,
+    family: 4 // Force IPv4 to bypass Render IPv6 routing issues
 });
 
 export default transporter;
